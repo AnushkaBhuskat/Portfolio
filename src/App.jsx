ae068a9b22fab0e0da492f6320384f69f29789e0
Anushka Bhuskat <anu.bhuskat@gmail.com>
2025-09-26 23:18:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/navbar";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Experience from "./components/Experience/experience";
