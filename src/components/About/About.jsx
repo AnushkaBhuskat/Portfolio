9e7b725c90e52739f3109ca129baa144e3d195b5
Anushka Bhuskat <anu.bhuskat@gmail.com>
2025-10-06 15:33:48 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Typewriter } from 'react-simple-typewriter';
 import Tilt from 'react-parallax-tilt';
-import My photo from '../../assets/My photo.jpg';
+import Myphoto from '../../assets/Myphoto.jpg';
 
 const About = () => {
   return (
@@ -58,7 +58,7 @@ const About = () => {
         <div className="md:w-1/2 flex justify-center md:justify-end">
           
             <img
-              src={My photo}
+              src={Myphoto}
               alt="Anushka Bhuskat"
               className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
